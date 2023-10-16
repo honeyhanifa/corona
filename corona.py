0ea2c1fd7d8a0d7a021ff229935a48673cdb2cd5
honeyhanifa <hanifatabassum28@gmail.com>
2023-10-16 20:37:37 +0530

    second commit

---
+++ corona.py
@@ -0,0 +1,14 @@
+from turtle import*
+color("green")
+bgcolor("black")
+#speed(12)
+speed(40)
+#hideturtle()
+#b=0
+#while(b<200):
+ #   right(b)
+  #  forward(b*3)
+   # b=b+1
+for i in range(210):
+    fd(i)
+    lt(i)
